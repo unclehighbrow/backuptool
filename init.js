1997740523a4e4db733218e32a8e4c1aff0b10b0
Will Carlough <will@carlough.net>
2025-03-18 19:27:28 -0400

    proper init

--- init.js
+++ init.js
@@ -1,18 +1,55 @@
 import _db from "./db.js";
 
-const init = async (db) => {
-  await db.query(`
-        CREATE TABLE IF NOT EXISTS file (
-            id SERIAL PRIMARY KEY,
-            path TEXT NOT NULL,
-            contents TEXT NOT NULL
-        );
-    `);
-};
+const init = async (dbName = "backuptool") => {
+  // create database if it's not already there
+  const initDb = await _db();
+  const result = await initDb.query(`
+    SELECT 1 FROM pg_database WHERE datname = '${dbName}';
+  `);
 
-(async () => {
-  const db = await _db("backuptool");
-  await init(db);
-})();
+  if (result.rowCount === 0) {
+    await initDb.query(`
+      CREATE DATABASE ${dbName};
+    `);
+  }
+
+  await initDb.end();
+
+  const db = await _db(dbName);
+
+  // create tables
+  await db.query(`
+    DROP TABLE IF EXISTS snapshot_file;
+    DROP TABLE IF EXISTS file;
+    DROP TABLE IF EXISTS snapshot;
+
+    CREATE TABLE file (
+      id SERIAL PRIMARY KEY,
+      sha varchar(255) NOT NULL,
+      contents BYTEA NOT NULL
+    );
+
+    CREATE TABLE snapshot (
+      id SERIAL PRIMARY KEY,
+      created_at timestamp without time zone DEFAULT now(),
+      directory_name character varying(255) NOT NULL
+    );
+
+    CREATE TABLE snapshot_file (
+      id SERIAL PRIMARY KEY,
+      path varchar(255) NOT NULL,
+      file_id integer NOT NULL,
+      snapshot_id integer NOT NULL
+    );
+
+    ALTER TABLE snapshot_file
+      ADD FOREIGN KEY (file_id) REFERENCES file (id); 
+    
+    ALTER TABLE snapshot_file
+      ADD FOREIGN KEY (snapshot_id) REFERENCES snapshot (id);
+  `);
+
+  await db.end();
+};
 
 export default init;
--- test/index.js
+++ test/index.js
@@ -1,19 +1,10 @@
 import _db from "../db.js";
 import init from "../init.js";
+import snapshot from "../commands/snapshot.js";
 
 (async () => {
-  const initDb = await _db();
-  const result = await initDb.query(`
-    SELECT 1 FROM pg_database WHERE datname = 'backuptool_test';
-`);
-
-  if (result.rowCount === 0) {
-    await initDb.query(`
-        CREATE DATABASE backuptool_test;
-    `);
-  }
-
+  await init("backuptool_test");
   const db = await _db("backuptool_test");
-  await init(db);
-  await db.end();
+  snapshot(db, "/Users/will/Desktop/snapshot1");
+  db.end();
 })();
