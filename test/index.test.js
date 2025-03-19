1ff119fc3ed9d2f5884341321c5eb7113ec83c20
Will Carlough <will@carlough.net>
2025-03-19 10:56:35 -0400

    surely you jest, i'm under cardiac arrest

---
+++ test/index.test.js
@@ -0,0 +1,19 @@
+import _db from "../db.js";
+import init from "../init.js";
+import snapshot from "../commands/snapshot.js";
+
+beforeEach(async () => {
+  await init("backuptool_test");
+});
+
+test("snapshot", async () => {
+  const db = await _db("backuptool_test");
+  await snapshot(db, "test/test_folders/snapshot1");
+  const snapshotResult = await db.query("SELECT * FROM snapshot");
+  expect(snapshotResult.rows.length).toBe(1);
+  const snapshotFileResult = await db.query("SELECT * FROM snapshot_file");
+  expect(snapshotFileResult.rows.length).toBe(8);
+  const fileResult = await db.query("SELECT * FROM file");
+  expect(fileResult.rows.length).toBe(7);
+  db.end();
+});
